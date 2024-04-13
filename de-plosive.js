93c2c1ee16ab950e3dc1dbb2022447056e9d7d3f
John Garcia <166593810+johnalvarogarcia@users.noreply.github.com>
2024-04-13 16:22:20 +1200

    Create de-plosive.js

---
+++ de-plosive.js
@@ -0,0 +1,10 @@
+
+//Calling command "Open, Render and Close AudioSuite Plugin" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckoo6b1uu000myj10s1rc02md',
+    props: {
+        category: "Noise Reduction",
+        pluginName: "RX 10 De-plosive",
+    }
+});
+
